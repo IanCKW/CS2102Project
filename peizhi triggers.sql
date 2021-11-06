ec6c419363d26e9cf396ba836b99b48f1fdc644d
ianckw <iankwchan@gmail.com>
2021-11-06 15:35:00 +0800

    fixed merge

--- peizhi triggers.sql
+++ peizhi triggers.sql
@@ -1,3 +1,6 @@
+DROP TRIGGER IF EXISTS room_in_updates ON Employees;
+DROP TRIGGER IF EXISTS delete_approved_joins ON Joins;
+
 
 CREATE OR REPLACE FUNCTION in_updates() 
 RETURNS TRIGGER AS $$
@@ -57,36 +60,55 @@ CREATE TRIGGER want_to_delete_dept
 BEFORE DELETE ON Departments
 FOR EACH ROW EXECUTE FUNCTION check_dept_relations();
 
-CREATE OR REPLACE FUNCTION leave_approved_meetings()
+CREATE OR REPLACE FUNCTION FOR EACH ROW EXECUTE FUNCTION leave_approved_meetings();
+()
 RETURNS TRIGGER AS $$
 DECLARE
     number_of_sick INTEGER;
+    join_approved INTEGER;
+    join_session INTEGER;
 BEGIN
+    SELECT COUNT(*) INTO join_approved
+    FROM Approves a
+    WHERE a.time = NEW.time
+    AND a.date = NEW.date
+    AND a.room = NEW.room
+    AND a.floor = NEW.floor
+    AND a.b_eid = NEW.b_eid;
+
+    SELECT COUNT(*) INTO join_session
+    FROM Sessions s
+    WHERE s.time = NEW.time
+    AND s.date = NEW.date
+    AND s.room = NEW.room
+    AND s.floor = NEW.floor
+    AND s.b_eid = NEW.b_eid;
 
     SELECT COUNT (*) INTO number_of_sick
     FROM (Health_Declarations h 
         JOIN contact_tracing(NEW.e_eid, NEW.date) c0 
-            ON h.eid = c0.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '1 day') c1 
-            ON h.eid = c1.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '2 day') c2 
-            ON h.eid = c2.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '3 day') c3 
-            ON h.eid = c3.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '4 day') c4 
-            ON h.eid = c4.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '5 day') c5 
-            ON h.eid = c5.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '6 day') c6 
-            ON h.eid = c6.e_eid
-        JOIN contact_tracing(NEW.e_eid, NEW.date - INTERVAL '7 day') c7 
-            ON h.eid = c7.e_eid
+            ON h.eid = c0
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '1 day' AS DATE ))  c1 
+            ON h.eid = c1
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '2 day' AS DATE ))  c2 
+            ON h.eid = c2
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '3 day' AS DATE ))  c3 
+            ON h.eid = c3
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '4 day' AS DATE ))  c4 
+            ON h.eid = c4
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '5 day' AS DATE ))  c5 
+            ON h.eid = c5
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '6 day' AS DATE ))  c6 
+            ON h.eid = c6
+        JOIN contact_tracing(NEW.e_eid, CAST ( NEW.date - INTERVAL '7 day' AS DATE ))  c7 
+            ON h.eid = c7
     ) hc
     WHERE hc.temp > 37.5;
     
-    IF number_of_sick > 0 or NEW.date > (SELECT resigned_date FROM Employees E where E.eid = NEW.e_eid) THEN
+    IF number_of_sick > 0 or NEW.date > (SELECT resigned_date FROM Employees E where E.eid = NEW.e_eid) 
+    or join_session = 0 or ( join_session >0 AND join_approved = 0) THEN 
         RAISE NOTICE 'Employee will be removed from Joins';
-        RETURN NEW;
+        RETURN OLD;
 
     ELSE
         RAISE NOTICE 'Cannot leave approved session';
